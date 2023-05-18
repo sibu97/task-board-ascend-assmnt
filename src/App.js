9b579ad72c20f97fd72bacf6a712158a8efdda04
Sibananda Nayak <nayaksibananda22@gmail.com>
2023-05-18 15:24:57 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { MultipleContainers} from '@dnd-kit';
+
 
 function App() {
   return (
     <div style={{ textAlign: 'center' }}>
     
-  <MultipleContainers
-    containerStyle={{
-      maxHeight: '80vh',
-    }}
-    itemCount={15}
-    scrollable
-  />
+ 
 
     </div>
   );
