19e6bd1ce6beb011074c30f15fe2f355238c9146
Sibananda Nayak <nayaksibananda22@gmail.com>
2023-05-18 15:16:59 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { MultipleContainers} from '@dnd-kit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center' }}>
+    
+  <MultipleContainers
+    containerStyle={{
+      maxHeight: '80vh',
+    }}
+    itemCount={15}
+    scrollable
+  />
+
     </div>
   );
 }
